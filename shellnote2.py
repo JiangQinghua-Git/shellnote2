eae11523ffdbd60147f98aa35a401782842651b5
mrtgst <johnmartingustavsson@gmail.com>
2020-10-28 19:19:57 +0100

    add timestamp

--- shellnote2.py
+++ shellnote2.py
@@ -2,10 +2,12 @@
 
 import argparse
 import os
+from time import strftime
 
 LOG_DIR = os.getenv("HOME") 
 LOG_FILE = "shellnote2.txt"
 LOG_PATH = LOG_DIR + "/" + LOG_FILE
+delim = "\t"
 
 def main():
     parser = argparse.ArgumentParser(description="shellnote2: easy note-taking on the command line.")
@@ -17,11 +19,13 @@ def main():
     args = parser.parse_args()
 
     if args.add:
-        note = args.add
+        date = strftime("%Y-%m-%d")
+        time = strftime("%H:%M")
+        entry = date+delim+time+delim+args.add
         with open(LOG_PATH, "a") as file:
-            file.write(note + "\n")
+            file.write(entry + "\n")
         if not args.quiet:
-            print(f"Entry added: {note}")
+            print(f"Entry added: {entry}")
 
 if __name__ == "__main__":
     main()
