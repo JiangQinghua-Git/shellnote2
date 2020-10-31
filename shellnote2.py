ee4f30a0784512edd16a71d0fc53512ee3cc17d0
mrtgst <johnmartingustavsson@gmail.com>
2020-10-31 15:50:34 +0100

    add --input flag and add_note() function

--- shellnote2.py
+++ shellnote2.py
@@ -9,6 +9,13 @@ LOG_FILE = "shellnote2.txt"
 LOG_PATH = LOG_DIR + "/" + LOG_FILE
 delim = "\t"
 
+def add_note(note):
+     date = strftime("%Y-%m-%d")
+     time = strftime("%H:%M")
+     entry = date+delim+time+delim+note
+     with open(LOG_PATH, "a") as file:
+         file.write(entry + "\n")
+
 def main():
     parser = argparse.ArgumentParser(description="shellnote2: easy note-taking on the command line.")
     
@@ -17,15 +24,12 @@ def main():
     parser.add_argument("-a", "--add", help="add note")
     parser.add_argument("-p", "--print", help="print entries", action="store_true")
     parser.add_argument("-e", "--edit", help="edit current entries in your text editor", action="store_true")
+    parser.add_argument("-i", "--input", help="add note by input prompt", action="store_true")
 
     args = parser.parse_args()
 
     if args.add:
-        date = strftime("%Y-%m-%d")
-        time = strftime("%H:%M")
-        entry = date+delim+time+delim+args.add
-        with open(LOG_PATH, "a") as file:
-            file.write(entry + "\n")
+        add_note(args.add)
         if not args.quiet:
             print(f"Added entry to {LOG_PATH}")
     
@@ -38,6 +42,12 @@ def main():
             os.system(f"$EDITOR {LOG_PATH}")
         else:
             os.system(f"vim {LOG_PATH}")
+    
+    if args.input:
+        note = input("Note: ")
+        add_note(note)
+        if not args.quiet:
+            print(f"Added entry to {LOG_PATH}")
 
 if __name__ == "__main__":
     main()
